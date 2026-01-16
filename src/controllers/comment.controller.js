6928a8f3700f4995d705f243973432eae6715888
tosif66 <motosifansari66@gmail.com>
2026-01-16 22:02:33 +0530

    feat: add comment functionalities to do crud on comments

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -4,6 +4,7 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import { ApiError } from "../utils/ApiError.js"
 
 import { asyncHandler } from "../utils/asyncHandlers.js";
+import { Video } from "../models/video.model.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
@@ -21,9 +22,9 @@ const getVideoComments = asyncHandler(async (req, res) => {
         .sort({ createdAt: -1 })
         .skip(skip)
         .limit(limit)
-        .populate("user", "username avatar")
+        .populate("owner", "username avatar")
 
-    const totalComments = await Comment.countDocuments({video: videoId});
+    const totalComments = await Comment.countDocuments({ video: videoId });
 
     const totalPages = Math.ceil(totalComments / limit);
 
@@ -33,7 +34,7 @@ const getVideoComments = asyncHandler(async (req, res) => {
         totalPages: totalPages,
         totalComments: totalComments,
         hasNextPage: page < totalPages,
-        hasPrevPage: page > 1 
+        hasPrevPage: page > 1
     }
 
     return res
@@ -44,34 +45,59 @@ const getVideoComments = asyncHandler(async (req, res) => {
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
     const { videoId } = req.params
-    if (!videoId) {
-        throw new ApiError(400, "video id not fount")
-    }
-    const comment = await Comment.findByIdAndUpdate(videoId)
+    const { comment } = req.body
+
     if (!comment) {
-        throw new ApiError(400, "Can not add comment")
+        throw new ApiError(400, "Please add a comment or comment not found")
     }
+
+    const video = await Video.findById(videoId)
+
+    if (!video) {
+        throw new ApiError(400, "video not found")
+    }
+
+    const newComment = Comment.create({
+        content: comment,
+        video: videoId,
+        owner: req.user?._id
+    })
+
+    return res.status(200)
+        .json(new ApiResponse(200, newComment, "Comment added successfully"))
+
 
 })
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
     const { commentId } = req.params
+    const { comment } = req.body
 
     if (!commentId) {
         throw new ApiError(400, "Comment id not found")
     }
-
-    const comment = await Comment.findByIdAndUpdate(commentId)
-
     if (!comment) {
-        throw new ApiError(400, "Cannot find comment")
+        throw new ApiError(400, "Comment not found to update")
+    }
+
+    const newComment = await Comment.findByIdAndUpdate(
+        commentId, 
+        {
+            $set:{
+                comment
+            }
+        },
+        {new: true}
+    )
+
+    if (!newComment) {
+        throw new ApiError(400, "Something went wrong while updating comment")
     }
 
     return res
         .status(200)
-        .json(new ApiResponse(200, comment, "Comment updated"))
-
+        .json(new ApiResponse(200, newComment, "Comment updated"))
 
 })
 
@@ -83,7 +109,11 @@ const deleteComment = asyncHandler(async (req, res) => {
         throw new ApiError(400, "Comment Id not found")
     }
 
-    await Comment.findByIdAndDelete(commentId)
+    const comment = await Comment.findByIdAndDelete(commentId)
+
+    if(!comment){
+        throw new ApiError(400, "Comment not found")
+    }
 
     return res
         .status(200)
