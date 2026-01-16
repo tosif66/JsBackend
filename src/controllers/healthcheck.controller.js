804f7f37752d8bcfaaebbaa2bb0a99d14dd71f72
tosif66 <motosifansari66@gmail.com>
2026-01-16 22:09:32 +0530

    feat: implement healthcheck endpoint to return OK status with message

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,7 +1,10 @@
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandlers.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
     //TODO: build a healthcheck response that simply returns the OK status as json with a message
+    return res.status(200)
+    .json(new ApiResponse(200, {}, "Everything is ok health check done"))
 })
 
 export { healthcheck }
